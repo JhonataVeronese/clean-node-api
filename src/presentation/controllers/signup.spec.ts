5351e5c398401b1389aa0817cddd8314439feb5e
JhonataVeronse <jhonataveronese@gmail.com>
2022-08-29 20:40:27 -0300

    test: Should SignUpController returns 200 vif valid data is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -166,6 +166,28 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
   });
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password',
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password',
+    })
+  });
 })
 
 interface SutTypes {
@@ -193,8 +215,8 @@ const makeAddAccount = (): IAddAccount => {
       const fakeAccount = {
         id: 'valid_id',
         name: 'valid_name',
-        email: 'validemail@email.com',
-        password: 'password_valid'
+        email: 'valid_email@email.com',
+        password: 'valid_password',
       }
       return fakeAccount;
     }
