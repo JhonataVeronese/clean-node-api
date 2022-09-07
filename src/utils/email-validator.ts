5dd85864299dc060732e3be8fef1f35d8be85ca6
JhonataVeronse <jhonataveronese@gmail.com>
2022-09-07 09:37:50 -0300

    feat: ensure add dependence validator email

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,8 +1,7 @@
+import validator from "validator";
 import { IEmailValidator } from "../presentation/protocols/email-validation";
-
-
 export class EmailValidationAdapter implements IEmailValidator {
   isValid(email: string): boolean {
-    return false
+    return validator.isEmail(email)
   }
 }
