ae972030cce6c7b1c783907a66cc129f636b3c08
JhonataVeronse <jhonataveronese@gmail.com>
2022-09-13 06:33:29 -0300

    test: comite error

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -8,7 +8,7 @@ describe('Bcrypt Adapter', () => {
     const sut = new BcryptAdapter(salt);
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
-    expect(hashSpy).toHaveBeenCalledWith('any_value');
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
 
   })
 
